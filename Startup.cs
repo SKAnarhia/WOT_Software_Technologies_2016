e2891a2af14471a3e6b57f9307ce1895ef90630d
DimitarYosifov <yosifov_1985@abv.bg>
2016-08-27 19:01:59 +0300

    Blog with Posts

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoBlogWasteOfTimeTeam.Startup))]
+namespace DemoBlogWasteOfTimeTeam
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
