81e0e8db9688d8d0b2f6ede8ca465ed7f143c882
DimitarYosifov <yosifov_1985@abv.bg>
2016-08-28 21:19:11 +0300

    Seeded 2 users

--- DemoBlogWasteOfTimeTeam/DemoBlogWasteOfTimeTeam/Migrations/Configuration.cs
+++ DemoBlogWasteOfTimeTeam/DemoBlogWasteOfTimeTeam/Migrations/Configuration.cs
@@ -1,32 +1,68 @@
 namespace DemoBlogWasteOfTimeTeam.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<DemoBlogWasteOfTimeTeam.Models.ApplicationDbContext>
+    public sealed class Configuration : DbMigrationsConfiguration<DemoBlogWasteOfTimeTeam.Models.ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
-            ContextKey = "DemoBlogWasteOfTimeTeam.Models.ApplicationDbContext";
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(DemoBlogWasteOfTimeTeam.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            if (!roleManager.Roles.Any())
+            {
+                roleManager.Create(new IdentityRole { Name = "Regular"});
+                roleManager.Create(new IdentityRole { Name = "Administrator"});
+            }
+
+            context.SaveChanges();
+
+            var adminUser = userManager.Users.FirstOrDefault(x => x.Email == "admin@gmail.com");
+
+            if (adminUser == null)
+            {
+                var admin = new ApplicationUser
+                {
+                    Email = "admin@gmail.com",
+                    UserName = "admin@gmail.com",
+                    EmailConfirmed = true,
+                };
+
+                userManager.Create(admin, "123456q");
+                userManager.AddToRole(admin.Id, "Administrator");
+            }
+
+            context.SaveChanges();
+
+            var regularUser = userManager.Users.FirstOrDefault(x => x.Email == "regular@gmail.com");
+
+            if (regularUser == null)
+            {
+                var regular = new ApplicationUser
+                {
+                    Email = "regular@gmail.com",
+                    UserName = "regular@gmail.com",
+                    EmailConfirmed = true,
+                };
+
+                userManager.Create(regular, "123456q");
+                userManager.AddToRole(regular.Id, "Regular");
+                regularUser = regular;
+            }
+
+            context.SaveChanges();
         }
     }
 }
